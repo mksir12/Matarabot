076ff99af5a5ffba1ab59bbf75a24a23d934867b
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-07-22 11:09:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/4450c7c8a397856cbc0d2.jpg https://telegra.ph/file/72f546d34d9aa825b9dec.jpg https://telegra.ph/file/2e3ef4779d153d6eac5be.jpg https://telegra.ph/file/bf2d9ecb8c06464e70e56.jpg https://telegra.ph/file/1e3a15ca6f9c38a728a3e.jpg https://telegra.ph/file/8601a64b75ed9202a6fdc.jpg https://telegra.ph/file/b847453762e656c1b84e5.jpg https://telegra.ph/file/ef06c9d5b78766dfc40cb.jpg https://telegra.ph/file/de3f6cff2b7f1d5df526b.jpg https://telegra.ph/file/1c0c8df531fcec1a2a7ce.jpg https://telegra.ph/file/42ae6ccf371896e19fbb2.jpg https://telegra.ph/file/5b3b62b68e9e75857b357.jpg https://telegra.ph/file/f98a3eb8a3ce0dc6c37c1.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/5786ba704fd0015d537c4.jpg")
-SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/b58f576fed14cd645d2cf.jpg")
+SPELL_IMG = environ.get('SPELL_IMG',"https://graph.org/file/922e47bdbd828726e1fc3.jpg")
 
 CLOSE_IMG = (environ.get('CLOSE_IMG', 'https://telegra.ph/file/6e9dd701bac49632cf79a.jpg https://telegra.ph/file/998d2b84e1411ed5189e3.jpg https://telegra.ph/file/c199babd469011d07f139.jpg https://telegra.ph/file/31b6d3d2c70bbe52b5300.jpg https://telegra.ph/file/77744524fbb6305298d45.jpg https://telegra.ph/file/9d79d990674166a2a2364.jpg')).split()
 
